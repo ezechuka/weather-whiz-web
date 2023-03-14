49145da680796b01d0312c4d9ac54976876e411f
ezechuka <ezechuka9@gmail.com>
2023-03-14 23:45:19 +0100

    feat: Wrap main component with chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,22 @@ import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "@fontsource/inter/100.css";
+import "@fontsource/inter/200.css";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/900.css";
+import { theme } from "./theme/Theme";
+import { WeatherContextProvider } from "./components/sidebar/WeatherContext";
+import { LocationContextProvider } from "./components/sidebar/LocationContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>
